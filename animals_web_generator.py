e6d3107004d59fea7b11dab88bd58c731a29e959
Saskia-K-S <saskia.katharina.sack@gmail.com>
2024-10-29 12:43:07 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,37 +8,27 @@ def load_data(file_path):
 animals_data = load_data('animals_data.json')
 
 print(animals_data)
-"""
-for animal in animals_data:
-    if 'name' in animal:
-        print(f"Name: {animal.get('name')}")
-    if 'diet' in animal['characteristics']:
-        print(f"Diet: {animal['characteristics']['diet']}")
-    if 'locations' in animal:
-        print(f"Location: {animal['locations'][0]}")
-    if 'type' in animal['characteristics']:
-        print(f"Type: {animal['characteristics']['type']}")
-    print()
-"""
+
 with open("animals_template.html", "r") as file:
     html_file = file.read()
 
-output = ''  # define an empty string
-for animal in animals_data:
-    # append information to each string
-    if 'name' in animal:
-        output += f"Name: {animal['name']}\n"
-    if 'diet' in animal['characteristics']:
-        output += f"Diet: {animal['characteristics']['diet']}\n"
-    if 'locations' in animal:
-        output += f"Location: {animal['locations'][0]}\n"
-    if 'type' in animal['characteristics']:
-        output += f"Type: {animal['characteristics']['type']}\n"
-    output += "\n"
-print(output)
+    output = ''  # define an empty string
+    for animal in animals_data:
+        # append information to each string
+        output += '<li class="cards__item">'
+        if 'name' in animal:
+            output += f"Name: {animal['name']}<br/>\n"
+        if 'diet' in animal['characteristics']:
+            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+        if 'locations' in animal:
+            output += f"Location: {animal['locations'][0]}<br/>\n"
+        if 'type' in animal['characteristics']:
+            output += f"Type: {animal['characteristics']['type']}<br/>\n"
+        output += '</li>'
+        output += "\n"
+    print(html_file)
+    updated_html = html_file.replace("__REPLACE_ANIMALS_INFO__", output)
 
-
-html_file.replace("__REPLACE_ANIMALS_INFO__", output)
 
 with open("animals.html", "w") as animals_html:
-    animals_html.write(html_file)
+    animals_html.write(updated_html)
