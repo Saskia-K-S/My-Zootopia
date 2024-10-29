5ef9598e615a5941196057dbade31aff90bcf3e2
Saskia-K-S <saskia.katharina.sack@gmail.com>
2024-10-29 12:22:43 +0100

    added animal data from html-file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,7 +8,7 @@ def load_data(file_path):
 animals_data = load_data('animals_data.json')
 
 print(animals_data)
-
+"""
 for animal in animals_data:
     if 'name' in animal:
         print(f"Name: {animal.get('name')}")
@@ -19,3 +19,26 @@ for animal in animals_data:
     if 'type' in animal['characteristics']:
         print(f"Type: {animal['characteristics']['type']}")
     print()
+"""
+with open("animals_template.html", "r") as file:
+    html_file = file.read()
+
+output = ''  # define an empty string
+for animal in animals_data:
+    # append information to each string
+    if 'name' in animal:
+        output += f"Name: {animal['name']}\n"
+    if 'diet' in animal['characteristics']:
+        output += f"Diet: {animal['characteristics']['diet']}\n"
+    if 'locations' in animal:
+        output += f"Location: {animal['locations'][0]}\n"
+    if 'type' in animal['characteristics']:
+        output += f"Type: {animal['characteristics']['type']}\n"
+    output += "\n"
+print(output)
+
+
+html_file.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w") as animals_html:
+    animals_html.write(html_file)
